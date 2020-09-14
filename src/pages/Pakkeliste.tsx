826b2c1eb0048f2e488bc9eb946fa0e98b451b63
Gaute Solbu Kleiven <gautesk@gmail.com>
2020-09-14 19:13:36 +0200

    Oppdater pakkeliste

--- src/pages/Pakkeliste.tsx
+++ src/pages/Pakkeliste.tsx
@@ -6,7 +6,16 @@ export default function Pakkeliste() {
         <>
             <h2>Pakkeliste</h2>
             <ul className="pakkeliste-wrapper">
-                ... Kommer snart!
+                <li>Sengetøy</li>
+                <li>Enkeltlaken (Larry/Magna: Dobbeltlaken)</li>
+                <li>Håndkle</li>
+                <li>Toalettsaker</li>
+                <li>Varme klær for utendørsleker (husk at det er høst, det kan bli kjølig å stå i ro)</li>
+                <li>Klær egnet for en liten (og bratt) gåtur</li>
+                <li>Klær egnet for innendørshygge</li>
+                <li>Termokopp (dersom du vil ha med deg kaffe ut)</li>
+                <li>Vannflaske</li>
+                <li>Alkohol. Vi ser for oss kosedrikking fredag, kanskje noen birras under konkurransene lørdag og at man får brukbar promille lørdag kveld. Suit yourself.</li>
             </ul>
         </>
     )
